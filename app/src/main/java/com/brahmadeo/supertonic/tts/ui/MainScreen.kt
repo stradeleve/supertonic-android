d21aed5701a728651649160f609c647003735783
Pankaj Kumar <pankaj.pal9@gmail.com>
2026-02-17 17:19:32 +0530

    chore: restore PC-compatible build configuration for PR

--- app/src/main/java/com/brahmadeo/supertonic/tts/ui/MainScreen.kt
+++ app/src/main/java/com/brahmadeo/supertonic/tts/ui/MainScreen.kt
@@ -308,7 +308,7 @@ fun DropdownSelector(
             label = { Text(label) },
             trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
             modifier = Modifier
-                .menuAnchor()
+                .menuAnchor(ExposedDropdownMenuAnchorType.PrimaryNotEditable)
                 .fillMaxWidth()
         )
         ExposedDropdownMenu(
